63d09e7ab01a7ff8aa9fe2c26a350fbc17aab5ba
kali <kali@kali.kali>
2025-11-05 12:37:57 -0600

    Modified .py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	# Change the message to something unique
-	return jsonify({"message": "it works! Student XXX"})
+	return jsonify({"message": "Kali hates me! Student 16!"})
 
 if __name__ == "__main__":
 	pp.run(threaded=True, host= '0.0.0.0', port=3000)
